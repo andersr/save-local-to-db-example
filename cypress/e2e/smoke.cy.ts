d296988757d45c0927ce9af0161172e6f8166a21
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2023-08-31 13:30:26 -0400

    debug e2e test, re-add remaining

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -27,10 +27,10 @@ describe("smoke tests", () => {
   });
 
   it("should allow you to make a note", () => {
-    // const testNote = {
-    //   title: faker.lorem.words(1),
-    //   body: faker.lorem.sentences(1),
-    // };
+    const testNote = {
+      title: faker.lorem.words(1),
+      body: faker.lorem.sentences(1),
+    };
     cy.login();
 
     cy.visitAndCheck("/notes");
@@ -40,12 +40,12 @@ describe("smoke tests", () => {
 
     cy.findByRole("link", { name: /new note/i }).click();
 
-    // cy.findByRole("textbox", { name: /title/i }).type(testNote.title);
-    // cy.findByRole("textbox", { name: /body/i }).type(testNote.body);
-    // cy.findByRole("button", { name: /save/i }).click();
+    cy.findByRole("textbox", { name: /title/i }).type(testNote.title);
+    cy.findByRole("textbox", { name: /body/i }).type(testNote.body);
+    cy.findByRole("button", { name: /save/i }).click();
 
-    // cy.findByRole("button", { name: /delete/i }).click();
+    cy.findByRole("button", { name: /delete/i }).click();
 
-    // cy.findByText("No notes yet");
+    cy.findByText("No notes yet");
   });
 });
