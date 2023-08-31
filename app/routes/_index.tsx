ddb8d9e45b19ee5e136851b5d4a5110d2831b4df
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2023-08-31 12:12:45 -0400

    Homepage styling

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -19,17 +19,17 @@ export default function Index() {
 
   function displaySaveStatus() {
     if (noteHasContent) {
-      return <span>Saved locally</span>;
+      return (
+        <span>
+          Saved locally.{" "}
+          <Link to={`/login${setParam()}`} className="text-gray-400 underline">
+            Save to my notes
+          </Link>
+          .
+        </span>
+      );
     }
-    return (
-      <span>
-        Saved locally until{" "}
-        <Link to={`/login${setParam()}`} className="text-gray-400 underline">
-          login
-        </Link>
-        .
-      </span>
-    );
+    return "";
   }
 
   function setParam() {
@@ -39,18 +39,9 @@ export default function Index() {
   }
 
   return (
-    <main className="p-8 max-w-4xl mx-auto">
-      <div>
-        <textarea
-          className="border p-2 border-slate-300 block"
-          value={value}
-          rows={4}
-          onChange={(e) => setValue(e.target.value)}
-          placeholder="Write something..."
-        />
-        <div className="py-2 text-sm text-slate-500">{displaySaveStatus()}</div>
-      </div>
-      <div className="p-4">
+    <div className="flex h-full min-h-screen flex-col">
+      <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
+        <h1 className="text-3xl font-bold">Anon Note To Db Example</h1>
         {user ? (
           <div>
             <Link to="/notes" className="">
@@ -64,15 +55,35 @@ export default function Index() {
           </div>
         ) : (
           <div className="">
-            <Link to={`/join${setParam()}`} className="">
+            <Link
+              to={`/join${setParam()}`}
+              className="rounded bg-slate-600 px-4 py-2 text-blue-100 hover:bg-blue-500 active:bg-blue-600 mr-4"
+            >
               Sign up
             </Link>
-            <Link to={`/login${setParam()}`} className="">
+            <Link
+              to={`/login${setParam()}`}
+              className="rounded bg-slate-600 px-4 py-2 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+            >
               Log In
             </Link>
           </div>
         )}
-      </div>
-    </main>
+      </header>
+      <main className="h-full bg-white p-8">
+        <div className="w-[400px] mx-auto">
+          <textarea
+            className="w-full rounded-md border-2 border-blue-500 px-3 py-2 text-lg leading-6"
+            value={value}
+            rows={8}
+            onChange={(e) => setValue(e.target.value)}
+            placeholder="Write something..."
+          />
+          <div className="py-2 text-sm text-slate-500">
+            {displaySaveStatus()}
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
